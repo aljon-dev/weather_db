58658a38ad0dc3b7d77c4159d69d1f03a9b61fdf
Meow_Dev <aljonjimenez091200@gmail.com>
2025-04-10 20:10:18 +0800

    push

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,47 +1,64 @@
 <script lang="ts">
-    import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell } from 'flowbite-svelte';
+	import {
+		Table,
+		TableBody,
+		TableBodyCell,
+		TableBodyRow,
+		TableHead,
+		TableHeadCell,
+		Navbar
+	} from 'flowbite-svelte';
 	import Chart from './Chart/Chart.svelte';
-	import PieChart from './Chart/PieChart.svelte';
 
+	let { data } = $props();
 
-    let {data} = $props();
+	function formatDate(dateString: string) {
+		const date = new Date(dateString);
+		const month = date.toLocaleString('default', { month: 'long' });
+		const day = date.getDate();
+		const year = date.getFullYear();
+		return `${month} ${day}, ${year}`;
+	}
 
-    let numberOfCountries = $state(0);
+	const formateTemperature = (Temperature) => {
+		return `${Math.round(((Temperature - 32) * 5) / 9)}°C`;
+	};
+	let numberOfCountries = $state(0);
+</script>
 
+<Navbar class="bg-slate-500">
+	<span class="text-lg font-medium text-white"> Weather Dashboard 2022 Record </span>
+</Navbar>
 
-</script> 
+<div class="flex grid w-full grid-cols-1 flex-col justify-center">
+	<div class="lg: flex flex-col justify-center gap-4 sm:flex-row sm:gap-8">
+		<Chart {data} />
+	</div>
+	<div class="mt-5 flex w-full justify-center">
+		<div class="w-full overflow-x-auto">
+			<Table class="min-w-full">
+				<TableHead>
+					<TableHeadCell>Country Code</TableHeadCell>
+					<TableHeadCell>City Name</TableHeadCell>
+					<TableHeadCell>Wind</TableHeadCell>
+					<TableHeadCell>Visibility</TableHeadCell>
+					<TableHeadCell>Date</TableHeadCell>
+					<TableHeadCell>Temperature</TableHeadCell>
+				</TableHead>
 
-
-<div class="flex flex-col grid grid-cols-1  w-full justify-center">
-    <div class="flex flex-col lg: gap-4 sm:flex-row sm:gap-8 justify-center">
-        <Chart {data} />
-        <PieChart {data} />
-      </div>
-    <div class="flex w-full justify-center mt-5">
-        <div class="overflow-x-auto w-full">
-            <Table class="min-w-full">
-                <TableHead>
-                    <TableHeadCell>Country Code</TableHeadCell>
-                    <TableHeadCell>City Name</TableHeadCell>
-                    <TableHeadCell>Wind</TableHeadCell>
-                    <TableHeadCell>Visibility</TableHeadCell>
-                    <TableHeadCell>Temperature</TableHeadCell>
-                </TableHead>
-
-                <TableBody tableBodyClass="divide-y">
-                    {#each data.WEATHER_DATA ?? [] as Weather}
-                        <TableBodyRow>
-                            <TableBodyCell>{Weather.COUNTRY_CODE}</TableBodyCell>
-                            <TableBodyCell>{Weather.CITY_NAME}</TableBodyCell>
-                            <TableBodyCell>{Weather.WIND_GUST_MIN}</TableBodyCell>
-                            <TableBodyCell>{Weather.VISIBILITY_MIN}</TableBodyCell>
-                            <TableBodyCell>{Weather.TEMPERATURE_REALFEEL_AVG}</TableBodyCell>
-                        </TableBodyRow>
-                    {/each}
-                </TableBody>
-            </Table>
+				<TableBody tableBodyClass="divide-y">
+					{#each data.WEATHER_DATA ?? [] as Weather}
+						<TableBodyRow>
+							<TableBodyCell>{Weather.COUNTRY_CODE}</TableBodyCell>
+							<TableBodyCell>{Weather.CITY_NAME}</TableBodyCell>
+							<TableBodyCell>{Weather.WIND_GUST_MIN}</TableBodyCell>
+							<TableBodyCell>{Weather.VISIBILITY_MIN}</TableBodyCell>
+							<TableBodyCell>{formatDate(Weather.DATE)}</TableBodyCell>
+							<TableBodyCell>{formateTemperature(Weather.TEMPERATURE_REALFEEL_AVG)}</TableBodyCell>
+						</TableBodyRow>
+					{/each}
+				</TableBody>
+			</Table>
+		</div>
+	</div>
 </div>
-   
-</div>
-</div>
-
--- src/routes/Chart/Chart.svelte
+++ src/routes/Chart/Chart.svelte
@@ -1,103 +1,108 @@
-<script  lang="ts">
-    import { Chart, Card, A, Button, Dropdown, DropdownItem } from 'flowbite-svelte';
-    import { ChevronRightOutline, ChevronDownOutline } from 'flowbite-svelte-icons';
-    
-    let {data} = $props();
+<script lang="ts">
+	import { Chart, Card, A, Button, Dropdown, DropdownItem } from 'flowbite-svelte';
+	import { ChevronRightOutline, ChevronDownOutline } from 'flowbite-svelte-icons';
 
+	let { data } = $props();
 
-    let options = {
-      chart: {
-        height: '400px',
-        maxWidth: '100%',
-        type: 'area' as const,
-        fontFamily: 'Inter, sans-serif',
-        dropShadow: {
-          enabled: false
-        },
-        toolbar: {
-          show: false
-        }
-      },
-      tooltip: {
-        enabled: true,
-        x: {
-          show: false
-        }
-      },
-      fill: {
-        type: 'gradient',
-        gradient: {
-          opacityFrom: 0.55,
-          opacityTo: 0,
-          shade: '#1C64F2',
-          gradientToColors: ['#1C64F2']
-        }
-      },
-      dataLabels: {
-        enabled: false
-      },
-      stroke: {
-        width: 6
-      },
-      grid: {
-        show: false,
-        strokeDashArray: 4,
-        padding: {
-          left: 2,
-          right: 2,
-          top: 0
-        }
-      },
-      series: [
-        {
-          name: 'Temperatures',
-          data: data.WEATHER_DATA.map((weather: any) => weather.TEMPERATURE_REALFEEL_AVG),
-          color: '#1A56DB'
-        }
-      ],
-      xaxis: {
-        categories: ['01 February', '02 February', '03 February', '04 February', '05 February', '06 February', '07 February'],
-        labels: {
-          show: false
-        },
-        axisBorder: {
-          show: false
-        },
-        axisTicks: {
-          show: false
-        }
-      },
-      yaxis: {
-        show: false
-      }
-    };
+	let options = {
+		chart: {
+			height: '400px',
+			maxWidth: '100%',
+			type: 'area' as const,
+			fontFamily: 'Inter, sans-serif',
+			dropShadow: {
+				enabled: false
+			},
+			toolbar: {
+				show: false
+			}
+		},
+		tooltip: {
+			enabled: true,
+			x: {
+				show: false
+			}
+		},
+		fill: {
+			type: 'gradient',
+			gradient: {
+				opacityFrom: 0.95,
+				opacityTo: 0,
+				shade: '#E81313',
+				gradientToColors: ['#E81313']
+			}
+		},
+		dataLabels: {
+			enabled: false
+		},
+		stroke: {
+			width: 6
+		},
+		grid: {
+			show: false,
+			strokeDashArray: 4,
+			padding: {
+				left: 2,
+				right: 2,
+				top: 0
+			}
+		},
+		series: [
+			{
+				name: 'Temperatures',
+				data: data.WEATHER_DATA.map(
+					(weather: any) => Math.round((weather.TEMPERATURE_REALFEEL_AVG - 32) * 5) / 9
+				),
+				color: '#e81313'
+			}
+		],
+		xaxis: {
+			categories: data.WEATHER_DATA.map((weather: any) => weather.COUNTRY_CODE),
+			labels: {
+				show: false
+			},
+			axisBorder: {
+				show: false
+			},
+			axisTicks: {
+				show: false
+			}
+		},
+		yaxis: {
+			show: false
+		}
+	};
 
-    const Temperatures = data.WEATHER_DATA.map((weather: any) => weather.TEMPERATURE_REALFEEL_AVG)
-    const highestTemp = Math.max(...Temperatures);
+	const Temperatures = data.WEATHER_DATA.map((weather: any) => weather.TEMPERATURE_REALFEEL_AVG);
+	const highestTemp = Math.max(...Temperatures);
+</script>
 
+<div class="w-full rounded-lg bg-white p-6 shadow dark:bg-gray-800">
+	<div class="flex justify-between">
+		<div>
+			<h5 class="pb-2 text-3xl leading-none font-bold text-gray-900 dark:text-white">
+				{Math.round(((highestTemp - 32) * 5) / 9)}°C
+			</h5>
+			<p class="text-base font-normal text-gray-500 dark:text-gray-400">Highest Temp</p>
+		</div>
+		<div
+			class="flex items-center px-2.5 py-0.5 text-center text-base font-semibold text-green-500 dark:text-green-500"
+		></div>
+	</div>
 
-  </script>
-  
-  <Card>
-    <div class="flex justify-between">
-      <div>
-        <h5 class="leading-none text-3xl font-bold text-gray-900 dark:text-white pb-2">{highestTemp}</h5>
-        <p class="text-base font-normal text-gray-500 dark:text-gray-400">Highest Temp</p>
-      </div>
-      <div class="flex items-center px-2.5 py-0.5 text-base font-semibold text-green-500 dark:text-green-500 text-center">
-        12%
-        <ChevronRightOutline class="w-6 h-6 ms-1" />
-      </div>
-    </div>
-    <Chart {options} />
-    <div class="grid grid-cols-1 items-center border-gray-200 border-t dark:border-gray-700 justify-between">
-      <div class="flex justify-between items-center pt-5">
-        <Button class="text-sm font-medium text-gray-500 dark:text-gray-400 hover:text-gray-900 text-center inline-flex items-center dark:hover:text-white bg-transparent hover:bg-transparent dark:bg-transparent dark:hover:bg-transparent focus:ring-transparent dark:focus:ring-transparent py-0">Last 7 days<ChevronDownOutline class="w-2.5 m-2.5 ms-1.5" /></Button>
-       
-        <A href="/" class="uppercase text-sm font-semibold hover:text-primary-700 dark:hover:text-primary-500 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 px-3 py-2 hover:no-underline">
-          Users Report
-          <ChevronRightOutline class="w-2.5 h-2.5 ms-1.5" />
-        </A>
-      </div>
-    </div>
-  </Card>
+	<!-- Make sure Chart container is full width -->
+	<div class="w-full">
+		<Chart {options} />
+	</div>
+
+	<div
+		class="grid grid-cols-1 items-center justify-between border-t border-gray-200 dark:border-gray-700"
+	>
+		<div class="flex items-center justify-between pt-5">
+			<A
+				href="/"
+				class="hover:text-primary-700 dark:hover:text-primary-500 rounded-lg px-3 py-2 text-sm font-semibold uppercase hover:bg-gray-100 hover:no-underline dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+			></A>
+		</div>
+	</div>
+</div>
